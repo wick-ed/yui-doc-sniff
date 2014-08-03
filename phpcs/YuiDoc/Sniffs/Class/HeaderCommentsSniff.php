8d7bd8c5aed92fba40f342f2926acd0441d79c7d
wick-ed <wick.b@hotmail.de>
2014-08-03 23:54:58 +0200

    updated class header sniff

--- phpcs/YuiDoc/Sniffs/Class/HeaderCommentsSniff.php
+++ phpcs/YuiDoc/Sniffs/Class/HeaderCommentsSniff.php
@@ -31,11 +31,31 @@ class YuiDoc_Sniffs_Class_HeaderCommentsSniff implements PHP_CodeSniffer_Sniff
     public $supportedTokenizers = array('JS');
     
     /**
-     * Counter to keep track of the occurences of the @module tag
+     * The tags which MUST be present
      * 
-     * @var int $counter
+     * @var array $neededTags
      */
-    protected $counter = 0;
+    protected $requiredTags = array('@class');
+    
+    /**
+     * The tags which of only ONE of them should be present
+     * 
+     * @var array $rivalingTags
+     */
+    protected $rivalingTags = array('@constructor', '@static');
+    
+    /**
+     * The tags which of only ONE of them should be present
+     * 
+     * @var array $relevantTags
+     */
+    protected $relevantTags = array(
+        '@class' => false, 
+        '@constructor' => false, 
+        '@static' => false,
+        '@extends' => false,
+        '@namespace' => false
+        );
 
     /**
      * Returns the token types that this sniff is interested in.
@@ -61,6 +81,57 @@ class YuiDoc_Sniffs_Class_HeaderCommentsSniff implements PHP_CodeSniffer_Sniff
         // get all the tokens we are interested in
         $tokens = $phpcsFile->getTokens();
 
+        // with the T_DOC_COMMENT_OPEN_TAG tag come the indexes of the other comment tags
+        $commentTagIndexes = $tokens[$stackPtr]['comment_tags'];
+        
+        // now iterate over all the indexes and get the tokens stored within them
+        foreach ($commentTagIndexes as $commentTagIndex) {
+            
+            // check if we got one of the header tags we want
+            $tagContent = $tokens[$commentTagIndex]['content'];
+            if (isset($this->relevantTags[$tagContent])) {
+                
+                $this->relevantTags[$tagContent] = true;
+            }
+        }
+
+        // did we get all of the required tags?
+        foreach ($this->requiredTags as $requiredTag) {
+            
+            // if we got the entry but it is not set we have to create an error
+            if ($this->relevantTags[$requiredTag] !== true) {
+                
+                $error = 'Missing required tag %s.';
+                $phpcsFile->addError($error, $stackPtr, 'Found', array($requiredTag));              
+            }
+        }
+        
+        // are there doubled rivaling tags?
+        $counter = 0;
+        foreach ($this->rivalingTags as $rivalingTag) {
+            
+            // if we got the entry we have to increment the counter
+            if ($this->relevantTags[$rivalingTag] === true) {
+                
+                $counter ++;              
+            }
+            
+            // if the counter is higher than 1 we got a problem
+            if ($counter > 1) {
+                
+                $error = 'Got several of the rivaling tags %s. '
+                        . 'There should be only one.';
+                $phpcsFile->addError(
+                        $error, 
+                        $stackPtr, 
+                        'Found', 
+                        array(implode(', ', $this->rivalingTags))
+                        );              
+            }
+        }
+        
+        
+        /*
         // collect the whole comment
         $docComment = '';
         for ($i = $stackPtr; $i < count($tokens); $i++) {
@@ -75,7 +146,7 @@ class YuiDoc_Sniffs_Class_HeaderCommentsSniff implements PHP_CodeSniffer_Sniff
                 break;
             }
         }
-error_log(var_export($docComment, true));
+
         // if we get the class name we have to check for the actual classname
         if (strpos($docComment, '@class') !== false) {
 
@@ -96,7 +167,6 @@ error_log(var_export($docComment, true));
                     $error = 'Mismatch of suggested (%s) and actual (%s) class name.';
                     $phpcsFile->addError($error, $stackPtr, 'Found', array($suggestedClassName, $actualClassName));
                 }
-            }
-        }
+    }*/
     }
 }
